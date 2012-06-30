0f4d1039df24a2918419218ca104aa967de47810
Steveice10 <Steveice10@gmail.com>
2012-06-29 21:40:22 -0700

    Incremented client version

--- src/main/java/ch/spacebase/openclassic/api/util/Constants.java
+++ src/main/java/ch/spacebase/openclassic/api/util/Constants.java
@@ -13,7 +13,7 @@ public class Constants {
 	/**
 	 * The client's current version.
 	 */
-	public static final String CLIENT_VERSION = "1.0.1";
+	public static final String CLIENT_VERSION = "1.0.2";
 	
 	/**
 	 * The server's protocol version.
