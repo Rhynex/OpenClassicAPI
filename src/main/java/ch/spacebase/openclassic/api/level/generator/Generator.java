5a054fd0c1daaa3e11c1259a78d5e2532222206e
Steveice10 <Steveice10@gmail.com>
2012-10-23 07:05:33 -0700

    Improved basic spawn finding

--- src/main/java/ch/spacebase/openclassic/api/level/generator/Generator.java
+++ src/main/java/ch/spacebase/openclassic/api/level/generator/Generator.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Random;
 
 import ch.spacebase.openclassic.api.Position;
+import ch.spacebase.openclassic.api.block.VanillaBlock;
 import ch.spacebase.openclassic.api.level.Level;
 import ch.spacebase.openclassic.api.util.storage.TripleIntByteArray;
 
@@ -37,17 +38,33 @@ public abstract class Generator {
 	public abstract String getName();
 	
 	/**
-	 * Finds a spawn for the level.
-	 * @param level The level to find a spawn for.
-	 * @return The spawn found for the level.
+	 * Finds the initial spawn for the level.
+	 * @param level The level to find the initial spawn for.
+	 * @return The initial spawn found for the level.
 	 */
-	public Position findSpawn(Level level) {
+	public Position findInitialSpawn(Level level) {
 		Random rand = new Random(level.getSeed());
-		int x = rand.nextInt(2000);
-		int y = 256;// / 2 + 12; // TODO
-		int z = rand.nextInt(2000);
+		return new Position(level, rand.nextInt(2000), 256, rand.nextInt(256));
+	}
+	
+	/**
+	 * Adjusts the level's spawn after the level has generated a chunk.
+	 * @param level The level to adjust the spawn for.
+	 * @return The adjusted spawn for the level.
+	 */
+	public Position adjustSpawn(Level level) {
+		int y = 256;
+		while(level.getBlockTypeAt(level.getSpawn().getBlockX(), y, level.getSpawn().getBlockZ()) == VanillaBlock.AIR) {
+			y--;
+			if(y <= 0) {
+				y = 256;
+				break;
+			}
+		}
 		
-		return new Position(level, x, y + 0.5f, z);	
+		Position newspawn = level.getSpawn().clone();
+		newspawn.setY(y + 1);
+		return newspawn;
 	}
 	
 }
