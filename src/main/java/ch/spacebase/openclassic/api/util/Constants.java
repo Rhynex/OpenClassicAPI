242057acf08c01475242ffe7c9bffde343bdeac5
Steveice10 <Steveice10@gmail.com>
2013-08-06 09:28:33 -0700

    Increment version

--- src/main/java/ch/spacebase/openclassic/api/util/Constants.java
+++ src/main/java/ch/spacebase/openclassic/api/util/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
 	/**
 	 * OpenClassic's current version.
 	 */
-	public static final String VERSION = "1.2.9";
+	public static final String VERSION = "1.3";
 	
 	/**
 	 * The protocol version.
