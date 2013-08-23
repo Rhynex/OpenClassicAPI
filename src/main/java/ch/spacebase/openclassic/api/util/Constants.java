eaefeb888ae3987bbe8f62c9d9df3e8ba96b770c
Steveice10 <Steveice10@gmail.com>
2013-08-22 18:09:59 -0700

    Increment to snapshot version

--- src/main/java/ch/spacebase/openclassic/api/util/Constants.java
+++ src/main/java/ch/spacebase/openclassic/api/util/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
 	/**
 	 * OpenClassic's current version.
 	 */
-	public static final String VERSION = "1.3.5";
+	public static final String VERSION = "1.3.6-SNAPSHOT";
 	
 	/**
 	 * The protocol version.
